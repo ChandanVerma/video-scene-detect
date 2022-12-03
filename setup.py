5d1a192dfa9dcc8517f5bb580cb6b6404a7da6f4
Chandan Verma <verma.chandan01@gmail.com>
2022-12-03 11:07:43 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'video_scene_detect',
+      version = '0.2',
+      description= 'Scene detection from video files(.mp4)',
+      author = 'Chandan Verma',
+      author_email = 'verma.chandan01@gmail.com',
+      packages = ['video_scene_detect'],
+      zip_safe = False)
